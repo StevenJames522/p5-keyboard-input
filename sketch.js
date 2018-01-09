ec7d1fa61996033c19bd8a3fb9d47eff1971991c
Steven James <sjames@wlapcs.org>
2018-01-09 18:50:30 +0000

    build keyboard inputs

--- sketch.js
+++ sketch.js
@@ -2,5 +2,27 @@ function setup(){
     createCanvas(600,400);
 }
 function draw(){
-background(50);
+    if(keyIsPressed){
+        blendMode(LIGHTEST);       
+        if(key=="r"){
+            background(255,0,0);
+        }
+        if(key=="g"){
+            background(0,255,0);           
+        }
+        if(key=="b"){
+            background(0,0,255);
+        }
+    
+        if(keyCode==32)
+            background(255,255,255);
+    }       
+       
+    else{
+        background(50,50,50);    
+    }
+    
+}
+function keyReleased(){
+    blendMode(NORMAL);
 }
